d4984683547495e5bb54692f8541acd0f9cdec86
LOHITHA <lohithagutha@gmail.com>
2025-09-20 07:20:44 +0530

    Project v2.0

--- STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
+++ STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
@@ -22,7 +22,13 @@ public class StudentController {
     @GetMapping("/")
     public String home() 
     {
-        return "Docker Full Stack Deployment Demo";
+        return "Docker Backend is Running";
+    }
+
+    @GetMapping("/docker")
+    public String docker() 
+    {
+        return "Docker Full Stack Deployment Demo with Git Actions";
     }
     
 
